335e03db7dae7d03da32b86b4f53ce5eac17b195
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-04 13:08:14 +0100

    ryddet i app.jsx

--- arbeidskrav_3/src/App.jsx
+++ arbeidskrav_3/src/App.jsx
@@ -1,10 +1,7 @@
 // --save-dev VIKTIG å skrive ved installering av pakker!!
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
 }
 
