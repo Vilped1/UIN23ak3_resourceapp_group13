28db78ecd5ed442cdaead8073e8e525cea39caad
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-04 13:15:18 +0100

    Litt sånne greier i nav

---
+++ arbeidskrav_3/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <ul>
+      <li className>Html</li>
+      <li>CSS</li>
+      <li>Javascript</li>
+      <li>React</li>
+      <li>Sanity og Headleass CMS</li>
+    </ul>
+  );
+}
