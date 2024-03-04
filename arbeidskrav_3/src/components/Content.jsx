620c6b509602c0ba8b8e3e6753e52054071662be
Vilped1 <vildeandrea01@gmail.com>
2024-03-04 14:42:38 +0100

    Filtrering
    Filtrere de forskjellige kategoriene uten bruk av trykk enda

---
+++ arbeidskrav_3/src/components/Content.jsx
@@ -0,0 +1,32 @@
+// import { useParams } from 'react-router-dom'
+import { resources } from '../resources/ressurser.js'
+
+export default function Content({ cat, setCat }) {
+
+    // const { slug } = useParams()
+
+    const filterCard = resources.filter(card => card.category === cat)
+
+    // useEffect(() => {
+    //     setCat(cat.filter(cat => cat.category === slug))
+    // })
+
+    return (
+        <>
+            {filterCard.map((card, index) =>
+                <article id="card" key={index}>
+                    <h1>{card.category}</h1>
+                    <p>{card.text}</p>
+                    <section>
+                        <ul>
+                            {card.sources.map((link, index) => 
+                                 <li key={index}><a href={link.url}>{link.title}</a></li>
+                            )}
+                        </ul>
+                    </section>
+                </article>
+            )}
+            
+        </>
+    )
+}
