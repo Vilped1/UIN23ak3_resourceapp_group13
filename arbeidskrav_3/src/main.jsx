ca9084d664e2f743e79ff17564c54b571f3e7a10
Vilped1 <vildeandrea01@gmail.com>
2024-03-06 08:01:41 +0100

    Update main.jsx

--- arbeidskrav_3/src/main.jsx
+++ arbeidskrav_3/src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import {BrowserRouter as Router} from "react-router-dom"
 // import "bootstrap/dist/css/bootstrap.css";
-// import "./styles/Nav.scss";
-// import "./styles/Main.scss";
+import "./styles/Nav.scss";
+import "./styles/Main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
