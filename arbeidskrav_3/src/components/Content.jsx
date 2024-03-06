0afdba70af3b3667c9d3aabb144c564681b03298
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-06 11:34:17 +0100

    Update Content.jsx

--- arbeidskrav_3/src/components/Content.jsx
+++ arbeidskrav_3/src/components/Content.jsx
@@ -1,11 +1,10 @@
-import { useParams } from "react-router-dom";
-import { resources } from "../resources/ressurser.js";
-import { useEffect } from "react";
+import { useParams } from 'react-router-dom'
+import { resources } from '../resources/ressurser.js'
+import { useEffect } from 'react'
 
-export default function Content({ cat, setCat }) {
-  // const { slug } = useParams()
-  // console.log(slug)
+export default function Content({ cat, setCat}) {
 
+<<<<<<< HEAD
   // ----> Ser ut som Sanity object blir filtrert ut her på et eller annet vis,
   // ----> dukker opp om man mapper direkte fra ressurser.js
   const filterCard = resources.filter((card) => card.category === cat);
@@ -13,11 +12,14 @@ export default function Content({ cat, setCat }) {
   console.log(filterCard);
   console.log(filterCard.length);
   console.log(resources);
+=======
+    // const { slug } = useParams()
+    // console.log(slug)
+>>>>>>> parent of 9c40409 (sanity skrives ikke ut, lagt til "nyttige" komment)
 
-  // useEffect(() => {
-  //     setCat(cat.filter(cat => cat.category === slug))
-  // }, [])
+    const filterCard = resources.filter(card => card.category === cat)
 
+<<<<<<< HEAD
   return (
     <>
       {filterCard.map((card, index) => (
@@ -38,3 +40,28 @@ export default function Content({ cat, setCat }) {
     </>
   );
 }
+=======
+    // useEffect(() => {
+    //     setCat(cat.filter(cat => cat.category === slug))
+    // }, [])
+
+    return (
+        <>
+            {filterCard.map((card, index) =>
+                <article id="card" key={index}>
+                    <h1>{card.category}</h1>
+                    <p>{card.text}</p>
+                    <section>
+                        <ul>
+                            {card.sources.map((link, index) => 
+                                 <li key={index}><a href={link.url}>{link.title}</a></li>
+                            )}
+                        </ul>
+                    </section>
+                </article>
+            )}
+            
+        </>
+    )
+}
+>>>>>>> parent of 9c40409 (sanity skrives ikke ut, lagt til "nyttige" komment)
