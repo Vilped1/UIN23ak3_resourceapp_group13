6a14503484fbc9ed4ce95204ef62f9a297432733
Vilped1 <vildeandrea01@gmail.com>
2024-03-06 11:30:56 +0100

    Update Nav.jsx
    Fiksa filtreringen av kategoriene

--- arbeidskrav_3/src/components/Nav.jsx
+++ arbeidskrav_3/src/components/Nav.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { resources } from "../resources/ressurser";
 
 export default function Nav({ cat, setCat }) {
+  // Ta vekk den
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const handleClick = (newCat) => {
@@ -17,11 +18,13 @@ export default function Nav({ cat, setCat }) {
             <li
               className={
                 selectedIndex === index ? "nav-button active" : "nav-button"
+                // cat === index.category ? "nav-button active" : "nav-button" Kan bruke denne
               }
               key={item}
               onClick={() => {
+                // Og ta vekk den
                 setSelectedIndex(index);
-                handleClick(item);
+                handleClick(item.category);
                 console.log(cat);
               }}
             >
