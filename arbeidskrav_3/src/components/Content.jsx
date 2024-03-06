4068a01690f954fd8126a3b4f3a55d4043b14cda
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-06 11:41:56 +0100

    router-dom savedev

--- arbeidskrav_3/src/components/Content.jsx
+++ arbeidskrav_3/src/components/Content.jsx
@@ -1,10 +1,8 @@
-import { useParams } from 'react-router-dom'
-import { resources } from '../resources/ressurser.js'
-import { useEffect } from 'react'
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { resources } from "../resources/ressurser.js";
 
-export default function Content({ cat, setCat}) {
-
-<<<<<<< HEAD
+export default function Content({ cat, setCat }) {
   // ----> Ser ut som Sanity object blir filtrert ut her på et eller annet vis,
   // ----> dukker opp om man mapper direkte fra ressurser.js
   const filterCard = resources.filter((card) => card.category === cat);
@@ -12,20 +10,17 @@ export default function Content({ cat, setCat}) {
   console.log(filterCard);
   console.log(filterCard.length);
   console.log(resources);
-=======
-    // const { slug } = useParams()
-    // console.log(slug)
->>>>>>> parent of 9c40409 (sanity skrives ikke ut, lagt til "nyttige" komment)
 
-    const filterCard = resources.filter(card => card.category === cat)
+  // useEffect(() => {
+  //     setCat(cat.filter(cat => cat.category === slug))
+  // }, [])
 
-<<<<<<< HEAD
   return (
     <>
       {filterCard.map((card, index) => (
         <article id="card" key={index}>
-          <h1>{card?.category}</h1>
-          <p>{card?.text}</p>
+          <h1>{card.category}</h1>
+          <p>{card.text}</p>
           <section>
             <ul>
               {card.sources.map((link, index) => (
@@ -40,28 +35,3 @@ export default function Content({ cat, setCat}) {
     </>
   );
 }
-=======
-    // useEffect(() => {
-    //     setCat(cat.filter(cat => cat.category === slug))
-    // }, [])
-
-    return (
-        <>
-            {filterCard.map((card, index) =>
-                <article id="card" key={index}>
-                    <h1>{card.category}</h1>
-                    <p>{card.text}</p>
-                    <section>
-                        <ul>
-                            {card.sources.map((link, index) => 
-                                 <li key={index}><a href={link.url}>{link.title}</a></li>
-                            )}
-                        </ul>
-                    </section>
-                </article>
-            )}
-            
-        </>
-    )
-}
->>>>>>> parent of 9c40409 (sanity skrives ikke ut, lagt til "nyttige" komment)
