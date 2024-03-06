9c4040925976c50a4e1a4a8c3c8c297b55523bd9
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-06 10:45:20 +0100

    sanity skrives ikke ut, lagt til "nyttige" komment

--- arbeidskrav_3/src/components/Content.jsx
+++ arbeidskrav_3/src/components/Content.jsx
@@ -1,34 +1,38 @@
-import { useParams } from 'react-router-dom'
-import { resources } from '../resources/ressurser.js'
-import { useEffect } from 'react'
+import { useParams } from "react-router-dom";
+import { resources } from "../resources/ressurser.js";
+import { useEffect } from "react";
 
-export default function Content({ cat, setCat}) {
+export default function Content({ cat, setCat }) {
+  // const { slug } = useParams()
+  // console.log(slug)
 
-    // const { slug } = useParams()
-    // console.log(slug)
+  // ----> Ser ut som Sanity object blir filtrert ut her på et eller annet vis,
+  // ----> dukker opp om man mapper direkte fra ressurser.js
+  const filterCard = resources.filter((card) => card.category === cat);
+  console.log(filterCard.length);
+  console.log(filterCard);
 
-    const filterCard = resources.filter(card => card.category === cat)
+  // useEffect(() => {
+  //     setCat(cat.filter(cat => cat.category === slug))
+  // }, [])
 
-    // useEffect(() => {
-    //     setCat(cat.filter(cat => cat.category === slug))
-    // }, [])
-
-    return (
-        <>
-            {filterCard.map((card, index) =>
-                <article id="card" key={index}>
-                    <h1>{card.category}</h1>
-                    <p>{card.text}</p>
-                    <section>
-                        <ul>
-                            {card.sources.map((link, index) => 
-                                 <li key={index}><a href={link.url}>{link.title}</a></li>
-                            )}
-                        </ul>
-                    </section>
-                </article>
-            )}
-            
-        </>
-    )
+  return (
+    <>
+      {filterCard.map((card, index) => (
+        <article id="card" key={index}>
+          <h1>{card.category}</h1>
+          <p>{card.text}</p>
+          <section>
+            <ul>
+              {card.sources.map((link, index) => (
+                <li key={index}>
+                  <a href={link.url}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </article>
+      ))}
+    </>
+  );
 }
