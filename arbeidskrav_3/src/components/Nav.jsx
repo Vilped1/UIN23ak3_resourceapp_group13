047374433e03cedeae87099ec70adce0debb0dd3
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-03-06 09:58:31 +0100

    nav bygger fra ressurser.js istedenfor statisk arr

--- arbeidskrav_3/src/components/Nav.jsx
+++ arbeidskrav_3/src/components/Nav.jsx
@@ -1,42 +1,38 @@
 import { useState } from "react";
+import { resources } from "../resources/ressurser";
 
-export default function Nav({cat, setCat}) { 
-  let navItems = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Sanity and headless CMS"
-  ];
-
+export default function Nav({ cat, setCat }) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const handleClick = (newCat) => {
-    setCat(newCat)
-  } 
+    setCat(newCat);
+  };
 
   return (
     <>
-    <nav> {/*!!*/}
-      <ul className="button-group">
-        {/* map function som mapper ut ifra array, gir og fjerner "active" styling etter trykk (Bootstrap)*/}
-        {navItems.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index ? "nav-button active" : "nav-button"
-            }
-            key={item}
-            onClick={() => {
-              setSelectedIndex(index);
-              handleClick(item)
-              console.log(cat)
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-      </nav> {/*!!*/}
+      <nav>
+        {" "}
+        {/*!!*/}
+        <ul className="button-group">
+          {/* map function som mapper ut ifra array, gir og fjerner "active" styling etter trykk (Bootstrap)*/}
+          {resources.map((item, index) => (
+            <li
+              className={
+                selectedIndex === index ? "nav-button active" : "nav-button"
+              }
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+                handleClick(item);
+                console.log(cat);
+              }}
+            >
+              {item.category}
+            </li>
+          ))}
+        </ul>
+      </nav>{" "}
+      {/*!!*/}
     </>
   );
 }
