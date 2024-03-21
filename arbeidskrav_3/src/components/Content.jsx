8e9fc17096fbdb65e7a1f36b2270a06f181cfb57
Vilped1 <vildeandrea01@gmail.com>
2024-03-21 10:23:35 +0100

    Kommentar

--- arbeidskrav_3/src/components/Content.jsx
+++ arbeidskrav_3/src/components/Content.jsx
@@ -2,19 +2,24 @@ import { resources } from '../resources/ressurser.js'
 
 export default function Content({ category }) {
 
+    // Filtrerer gjennom "resources" om å filtrere ut resurss kortene som har card.category som er lik "category"
     const filterCard = resources.filter(card => card.category === category)
 
     return (
         <>
+            {/* Går gjennom alle objektene i arrayen */}
             {filterCard.map((card, index) =>
+            // Legger inn korrekt HTML struktur for riktig visning av resurss kortene
                 <article key={index}>
                     <h1>{card.category}</h1>
                     <p>{card.text}</p>
                     <section>
                         <ul>
+                            {/* Går gjennom alle objektene i arrayen til hvert enkelt objekt som blir mappet */}
                             {card.sources.map((link, index) =>
                                 <li key={index}><a href={link.url}>{link.title}</a></li>
                             )}
+                        {/* Sørger for at taggene er avsluttet */}
                         </ul>
                     </section>
                 </article>
